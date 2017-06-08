1f75ba781ed64b8b78659bd78023b0ea2bdafee6
Your Name <you@example.com>
2017-06-08 15:32:01 +0300

    Bash.im bot

---
+++ bashimbot.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import telebot
+from telebot import types
+import re
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+BOT_TOKEN = '387480715:AAGE4QwIkF6OaTaCln8vB4eAWPwD8Nes_fM' # Your bot token
+
+bot = telebot.TeleBot(BOT_TOKEN)
+random.seed()
+
+
+def get_random_quote():
+	"""
+	Get random quote and parse text from bash.im
+	"""
+	rand_id = random.randint(440000, 445000)
+	url = "http://bash.im/quote/{}".format(str(rand_id))
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	text = str(soup.find("div", class_="text"))
+	text = re.sub(re.compile('<br>'), '\n', text)
+	text = re.sub(re.compile('<.*?>'), '', text)
+	return url + "\n\n" + text
+
+
+@bot.message_handler(commands=['start'])
+def cmd_start(message):
+	text = "Этот бот выдает рандомные цитати с сайта bash.im\n"
+	text += "Отпрвьте мне команду /quote, что бы получить цитату."
+	bot.send_message(message.chat.id, text)
+	return
+
+
+@bot.message_handler(commands=["quote"])
+def quote_commdnd(message):
+	keyboard = types.InlineKeyboardMarkup()
+	btn = types.InlineKeyboardButton(text="Ещё цитата", callback_data="quote")
+	keyboard.add(btn)
+	text = get_random_quote()
+	bot.send_message(message.chat.id, text, reply_markup=keyboard, parse_mode="html")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+	if call.message:
+		if call.data == "quote":
+			keyboard = types.InlineKeyboardMarkup()
+			btn = types.InlineKeyboardButton(text="Ещё цитата", callback_data="quote")
+			keyboard.add(btn)
+			text = get_random_quote()
+			while text == call.message.text:
+				text = get_random_quote()
+			bot.edit_message_text(
+				chat_id=call.message.chat.id, 
+				message_id=call.message.message_id, 
+				text=text, reply_markup=keyboard, parse_mode="html")
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
